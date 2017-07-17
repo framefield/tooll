122f89eda8e9f7f9c16d9e5cf59afc0b5eaa4528
cynic <daniel.szymanski@framefield.com>
2017-07-17 21:50:41 +0200

    Added light radius property to PBR point light interface

--- Core/Rendering/PointLight.cs
+++ Core/Rendering/PointLight.cs
@@ -87,6 +87,7 @@ namespace Framefield.Core
         Vector3 Position { get; }
         Color3 Color { get; }
         Vector3 Intensity { get; }
+        float LightRadius { get; }
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 32)]
@@ -96,13 +97,14 @@ namespace Framefield.Core
         {
             Position = new Vector4(pointLight.Position, 1);
             Color = pointLight.Color.ToVector3()*pointLight.Intensity;
+            LightRadius = pointLight.LightRadius;
         }
         [FieldOffset(0)]
         public Vector4 Position;
         [FieldOffset(16)]
         public Vector3 Color;
-//        [FieldOffset(28)]
-//        public float Dummy;
+        [FieldOffset(28)]
+        public float LightRadius;
     }
 
 }
