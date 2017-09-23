9fc09feeb969b75ed1907c035138f9a34362c06c
pixtur <thomas.mann@framefield.com>
2017-09-23 04:24:37 +0200

    First version of save sound-levels when generating sound images

---
+++ Core/Mathf/Mathf.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (c) 2016 Framefield. All rights reserved.
+// Released under the MIT license. (see LICENSE.txt)
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Diagnostics;
+using SharpDX;
+
+namespace Framefield.Core
+{
+
+    public static class Mathf
+    {
+        public static bool IsEqual(float lhs, float rhs, float epsilon = 0.001f)
+        {
+            return Math.Abs(lhs - rhs) < epsilon;
+        }
+
+        public static T Clamp<T>(T val, T min, T max) where T : IComparable<T>
+        {
+            return (val.CompareTo(min) < 0) ? min
+                                            : (val.CompareTo(max) > 0) ? max
+                                                                       : val;
+        }
+
+        public static float Clamp01(float val)
+        {
+            return (val <= 0f) ? 0f
+                             : (val >= 1f) ? 1f
+                                           : val;
+        }
+
+        public static float Max(float A, float B)
+        {
+            return (A >= B) ? A : B;
+        }
+
+        public static float Min(float A, float B)
+        {
+            return (A <= B) ? A : B;
+        }
+
+        public static float SmoothStep(float t)
+        {
+            return t * t * t * (t * (t * 6 - 15) + 10);
+        }
+
+        public static float getAngleDifference(float a1, float a2)
+        {
+            var d = (float)Math.Abs(a1 - a2);
+            return d > 180f ? 360f - d : d;
+        }
+
+        // t within [0, 1]
+        public static float Lerp(float a, float b, float t)
+        {
+            return a * (1.0f - t) + b * t;
+        }
+
+        public static float RadToDegree(float rad)
+        {
+            return rad * 180f / (float)Math.PI;
+        }
+
+        public static float DegreeToRad(float degree)
+        {
+            return degree * (float)Math.PI / 180f;
+        }
+    }
+}
--- Tooll/Components/Helper/GenerateSoundImage.cs
+++ Tooll/Components/Helper/GenerateSoundImage.cs
@@ -13,17 +13,18 @@ using System.Windows.Forms;
 using Framefield.Core;
 using Un4seen;
 using Un4seen.Bass;
+using Newtonsoft.Json;
 
 namespace Framefield.Tooll.Components.Helper
 {
-    public static class GenerateSoundImage
+    public class SoundImageGenerator
     {
-        public static string GenerateSoundSprectrumAndVolume(String soundFilePath)
+        public string GenerateSoundSpectrumAndVolume(String soundFilePath)
         {
-            if ( String.IsNullOrEmpty(soundFilePath) || !File.Exists(soundFilePath))
+            if (String.IsNullOrEmpty(soundFilePath) || !File.Exists(soundFilePath))
                 return null;
 
-            var imageFilePath = soundFilePath+".waveform.png";
+            var imageFilePath = soundFilePath + ".waveform.png";
             if (File.Exists(imageFilePath))
             {
                 Logger.Info("Reusing sound image file: {0}", imageFilePath);
@@ -31,7 +32,7 @@ namespace Framefield.Tooll.Components.Helper
             }
 
             Logger.Info("Generating {0}...", imageFilePath);
-            
+
             Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_LATENCY, IntPtr.Zero);
             var stream = Bass.BASS_StreamCreateFile(soundFilePath, 0, 0,
                                                     BASSFlag.BASS_STREAM_DECODE | BASSFlag.BASS_STREAM_PRESCAN);
@@ -39,21 +40,16 @@ namespace Framefield.Tooll.Components.Helper
             const int SAMPLING_FREQUENCY = 100; //warning: in TimelineImage this samplingfrequency is assumed to be 100
             const double SAMPLING_RESOLUTION = 1.0 / SAMPLING_FREQUENCY;
 
-            var sampleLength = Bass.BASS_ChannelSeconds2Bytes(stream, SAMPLING_RESOLUTION);            
+            var sampleLength = Bass.BASS_ChannelSeconds2Bytes(stream, SAMPLING_RESOLUTION);
             var numSamples = Bass.BASS_ChannelGetLength(stream, BASSMode.BASS_POS_BYTES) / sampleLength;
 
             Bass.BASS_ChannelPlay(stream, false);
 
-            const int spectrumLength = 1024;
-            const int imageHeight = 256;
-            var spectrumImage = new Bitmap((int)numSamples, imageHeight);
-            var volumeImage = new Bitmap((int)numSamples, imageHeight);
+            var spectrumImage = new Bitmap((int)numSamples, IMAGE_HEIGHT);
+            var volumeImage = new Bitmap((int)numSamples, IMAGE_HEIGHT);
 
-            var tempBuffer = new float[spectrumLength];
+            //var fftBuffer = new float[SPECTRUM_LENGTH];
 
-            const float maxIntensity = 500;
-            const int COLOR_STEPS = 255;
-            const int PALETTE_SIZE = 3 * COLOR_STEPS;
 
             int a, b, r, g;
             var palette = new Color[PALETTE_SIZE];
@@ -84,41 +80,111 @@ namespace Framefield.Tooll.Components.Helper
                 palette[palettePos] = Color.FromArgb(a, r, g, b);
             }
 
-            var f = (float)(spectrumLength / Math.Log((float)(imageHeight + 1)));
-            var f2 = (float)((PALETTE_SIZE - 1) / Math.Log(maxIntensity + 1));
-            var f3 = (float)((imageHeight - 1) / Math.Log(32768.0f + 1));
+            foreach (var region in REGIONS)
+            {
+                region.levels = new float[numSamples];
+            }
+
+            var f = (float)(SPECTRUM_LENGTH / Math.Log((float)(IMAGE_HEIGHT + 1)));
+            var f2 = (float)((PALETTE_SIZE - 1) / Math.Log(MAX_INTENSITY + 1));
+            var f3 = (float)((IMAGE_HEIGHT - 1) / Math.Log(32768.0f + 1));
 
             for (var sampleIndex = 0; sampleIndex < numSamples; ++sampleIndex)
             {
                 Bass.BASS_ChannelSetPosition(stream, sampleIndex * sampleLength, BASSMode.BASS_POS_BYTES);
-                Bass.BASS_ChannelGetData(stream, tempBuffer, (int)Un4seen.Bass.BASSData.BASS_DATA_FFT2048);
-               
-                for (var rowIndex = 0; rowIndex < imageHeight; ++rowIndex)
+                Bass.BASS_ChannelGetData(stream, _fftBuffer, (int)Un4seen.Bass.BASSData.BASS_DATA_FFT2048);
+
+                for (var rowIndex = 0; rowIndex < IMAGE_HEIGHT; ++rowIndex)
                 {
                     var j_ = (int)(f * Math.Log(rowIndex + 1));
                     var pj_ = (int)(rowIndex > 0 ? f * Math.Log(rowIndex - 1 + 1) : j_);
-                    var nj_ = (int)(rowIndex < imageHeight - 1 ? f * Math.Log(rowIndex + 1 + 1) : j_);
-                    float intensity = 125.0f * tempBuffer[spectrumLength - pj_ - 1] +
-                                      750.0f * tempBuffer[spectrumLength - j_ - 1] +
-                                      125.0f * tempBuffer[spectrumLength - nj_ - 1];
-                    intensity = Math.Min(maxIntensity, intensity);
+                    var nj_ = (int)(rowIndex < IMAGE_HEIGHT - 1 ? f * Math.Log(rowIndex + 1 + 1) : j_);
+                    float intensity = 125.0f * _fftBuffer[SPECTRUM_LENGTH - pj_ - 1] +
+                                      750.0f * _fftBuffer[SPECTRUM_LENGTH - j_ - 1] +
+                                      125.0f * _fftBuffer[SPECTRUM_LENGTH - nj_ - 1];
+                    intensity = Math.Min(MAX_INTENSITY, intensity);
                     intensity = Math.Max(0.0f, intensity);
 
                     palettePos = (int)(f2 * Math.Log(intensity + 1));
                     spectrumImage.SetPixel(sampleIndex, rowIndex, palette[palettePos]);
                 }
 
-                if (sampleIndex%1000 == 0)
+                if (sampleIndex % 1000 == 0)
                 {
-                    Logger.Info("   computing sound image {0}% complete", (100*sampleIndex/numSamples));
+                    Logger.Info("   computing sound image {0}% complete", (100 * sampleIndex / numSamples));
+                }
+
+                foreach (var region in REGIONS)
+                {
+                    region.ComputeUpLevelForCurrentFFT(sampleIndex, ref _fftBuffer);
                 }
             }
-            
+
+            foreach (var region in REGIONS)
+            {
+                region.SaveToFile(soundFilePath);
+            }
+
             spectrumImage.Save(imageFilePath);
             Bass.BASS_ChannelStop(stream);
             Bass.BASS_StreamFree(stream);
 
             return imageFilePath;
         }
+
+        internal class FftRegion
+        {
+            public string title;
+            public float[] levels;
+            public float lowerLimit;
+            public float upperLimit;
+
+            public void ComputeUpLevelForCurrentFFT(int index, ref float[] fftBuffer)
+            {
+                var level = 0f;
+
+                var startIndex = (int)Mathf.Lerp(0, SPECTRUM_LENGTH, Mathf.Clamp01(this.lowerLimit));
+                var endIndex = (int)Mathf.Min(SPECTRUM_LENGTH, Mathf.Lerp(0, SPECTRUM_LENGTH, Mathf.Clamp01(this.upperLimit)));
+
+
+
+
+                if (startIndex >= endIndex)
+                {
+                    levels[index] = 0;
+                    return;
+                }
+
+                for (int i = startIndex; i < endIndex; i++)
+                {
+                    level += fftBuffer[i];
+                }
+                levels[index] = level;
+            }
+
+            public void SaveToFile(string basePath)
+            {
+                using (var sw = new StreamWriter(basePath + "." + title + ".json"))
+                {
+                    sw.Write(JsonConvert.SerializeObject(levels, Formatting.Indented));
+                }
+            }
+        }
+
+        FftRegion[] REGIONS = new FftRegion[]
+        {
+            new FftRegion(){title = "levels", lowerLimit = 0f, upperLimit = 1f,},
+            new FftRegion(){title = "highlevels", lowerLimit = 0.3f, upperLimit = 1f,},
+            new FftRegion(){title = "midlevels", lowerLimit = 0.06f, upperLimit = 0.3f,},
+            new FftRegion(){title = "lowlevels", lowerLimit = 0.0f, upperLimit = 0.06f,},
+        };
+
+        const int SPECTRUM_LENGTH = 1024;
+        const int IMAGE_HEIGHT = 256;
+        const float MAX_INTENSITY = 500;
+        const int COLOR_STEPS = 255;
+        const int PALETTE_SIZE = 3 * COLOR_STEPS;
+
+        internal float[] _fftBuffer = new float[SPECTRUM_LENGTH];
     }
 }
